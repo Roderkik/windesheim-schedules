dff1a3c44d5ebea07121a5d2cee3dce9aeaa8180
Roderik Rasterhoff <S1125417@student.windesheim.nl>
2019-11-30 02:41:38 +0100

    Start of landing page

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/css/mdb.min.css" rel="stylesheet">
+    <title>Windeschedule - iCalendar generator</title>
+</head>
+<body class="d-flex">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-12 offset-sm-0 col-md-8 offset-md-2 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+            <div class="card card-coming-soon">
+                <div class="card-body">
+                    <div class="border-bottom">
+                        <h3 class="text-center card-title">Schedule selector coming soon&trade;</h3>
+                        <h6 class="text-center card-subtitle mb-2 text-muted">Create them yourself for now...</h6>
+                    </div>
+                    <div class="card-content">
+                        <span>URL structure:</span><br>
+                        <kbd>"windeschedule.nl/schedule/[type]/[code]"</kbd><br><br>
+                        <span>Examples:</span><br>
+                        <samp>Class:</samp>
+                        <a href="http://windeschedule.nl/schedule/class/ICTSE1e"><samp>windeschedule.nl/schedule/class/ICTSE1e</samp></a><br>
+                        <samp>Teacher:</samp>
+                        <a href="http://windeschedule.nl/schedule/teacher/BNH09"><samp>windeschedule.nl/schedule/teacher/BNH09</samp></a><br>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<style>
+    body {
+        height: 100vh;
+    }
+
+    .card-content {
+        margin-top: 1vh;
+    }
+
+    .card-coming-soon {
+        margin-top: 5vh;
+    }
+</style>
+<!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/js/mdb.min.js"></script>
+</body>
+</html>
